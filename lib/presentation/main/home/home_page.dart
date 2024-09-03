7e190708bc3f480b2638b25707182c3941f2dc00
lobarashurova <106048869+lobarashurova@users.noreply.github.com>
2024-09-03 13:43:59 +0500

    realtime chat completed

--- lib/presentation/main/home/home_page.dart
+++ lib/presentation/main/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:bank_mobile/extensions/navigation_extensions.dart';
 import 'package:bank_mobile/extensions/text_extensions.dart';
 import 'package:bank_mobile/extensions/theme_extensions.dart';
 import 'package:bank_mobile/extensions/widget.dart';
+import 'package:bank_mobile/presentation/chat/chat_page.dart';
 import 'package:bank_mobile/presentation/main/home/data/grid_item_data.dart';
 import 'package:bank_mobile/presentation/main/home/notifications/notifications_page.dart';
 import 'package:bank_mobile/presentation/main/home/payment/payment_page.dart';
@@ -73,143 +74,163 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    "Hello user!".s(20).c(Colors.white).w(700),
-                    IconButton(
-                        onPressed: () {
-                          context.push(NotificationsPage());
-                        },
-                        icon: Icon(
-                          CupertinoIcons.bell,
-                          color: Colors.white,
-                        ))
-                  ],
-                ),
-                16.kh,
-                GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    childAspectRatio: 1.1,
-                    crossAxisCount: 2, // Number of columns
-                    crossAxisSpacing: 10.0, // Space between columns
-                    mainAxisSpacing: 10.0, // Space between rows
-                  ),
-                  itemCount: items.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => items[index].widget));
-                      },
-                      child: Container(
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: context.colors.primary),
-                            borderRadius: BorderRadius.circular(10),
-                            color: context.colors.label,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.3),
-                                spreadRadius: 1,
-                                blurRadius: 3,
-                                offset: Offset(0, 3), // Offset of the shadow
-                              ),
-                            ],
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              items[index].icon,
-                              32.kh,
-                              Center(
-                                child: items[index].title,
-                              )
-                            ],
-                          )),
-                    );
-                  },
-                ),
-                16.kh,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    "News".w(600).s(25).c(Colors.white),
-                    10.kw,
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          expend = !expend;
-                        });
-                      },
-                      child: expend
-                          ? "expend".s(14).c(Colors.white).w(500)
-                          : "shorten".s(14).c(Colors.white).w(500),
-                    )
-                  ],
-                ),
-                16.kh,
-                ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: expend ? 2 : 10,
-                    itemBuilder: (context, i) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        child: Container(
-                          alignment: Alignment.topLeft,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 10),
-                          decoration: BoxDecoration(
-                              color: Colors.deepPurpleAccent.withOpacity(0.1),
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(10))),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  const Badge(
-                                    backgroundColor: Colors.green,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        "Hello user!".s(20).c(Colors.white).w(700),
+                        IconButton(
+                            onPressed: () {
+                              context.push(NotificationsPage());
+                            },
+                            icon: Icon(
+                              CupertinoIcons.bell,
+                              color: Colors.white,
+                            ))
+                      ],
+                    ),
+                    16.kh,
+                    GridView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        childAspectRatio: 1.1,
+                        crossAxisCount: 2, // Number of columns
+                        crossAxisSpacing: 10.0, // Space between columns
+                        mainAxisSpacing: 10.0, // Space between rows
+                      ),
+                      itemCount: items.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => items[index].widget));
+                          },
+                          child: Container(
+                              padding: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: context.colors.primary),
+                                borderRadius: BorderRadius.circular(10),
+                                color: context.colors.label,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.3),
+                                    spreadRadius: 1,
+                                    blurRadius: 3,
+                                    offset:
+                                        Offset(0, 3), // Offset of the shadow
                                   ),
-                                  " News".s(18).c(Colors.green),
                                 ],
                               ),
-                              10.kh,
-                              "Асадбек Тоштемиров завоевал четвертую золотую медаль Паралимпийских игр для Узбекистана."
-                                  .s(13)
-                                  .c(Colors.white),
-                              10.kh,
-                              "01.08.2024".s(13).c(Colors.white70)
-                            ],
-                          ),
-                        ),
-                      );
-                    }),
-                16.kh,
-                "Outcomes".s(14).w(600).c(context.colors.onPrimary),
-                16.kh,
-                OutcomeLine(),
-                "Incomes".s(14).w(600).c(context.colors.onPrimary),
-                16.kh,
-                InComeLine(),
-                16.kh,
-                "Daily benefit".s(14).w(600).c(context.colors.onPrimary),
-                32.kh,
-                BarWidget()
-              ],
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  items[index].icon,
+                                  32.kh,
+                                  Center(
+                                    child: items[index].title,
+                                  )
+                                ],
+                              )),
+                        );
+                      },
+                    ),
+                    16.kh,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        "News".w(600).s(25).c(Colors.white),
+                        10.kw,
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              expend = !expend;
+                            });
+                          },
+                          child: expend
+                              ? "expend".s(14).c(Colors.white).w(500)
+                              : "shorten".s(14).c(Colors.white).w(500),
+                        )
+                      ],
+                    ),
+                    16.kh,
+                    ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: expend ? 2 : 10,
+                        itemBuilder: (context, i) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Container(
+                              alignment: Alignment.topLeft,
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 10),
+                              decoration: BoxDecoration(
+                                  color:
+                                      Colors.deepPurpleAccent.withOpacity(0.1),
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(10))),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      const Badge(
+                                        backgroundColor: Colors.green,
+                                      ),
+                                      " News".s(18).c(Colors.green),
+                                    ],
+                                  ),
+                                  10.kh,
+                                  "Асадбек Тоштемиров завоевал четвертую золотую медаль Паралимпийских игр для Узбекистана."
+                                      .s(13)
+                                      .c(Colors.white),
+                                  10.kh,
+                                  "01.08.2024".s(13).c(Colors.white70)
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                    16.kh,
+                    "Outcomes".s(14).w(600).c(context.colors.onPrimary),
+                    16.kh,
+                    OutcomeLine(),
+                    "Incomes".s(14).w(600).c(context.colors.onPrimary),
+                    16.kh,
+                    InComeLine(),
+                    16.kh,
+                    "Daily benefit".s(14).w(600).c(context.colors.onPrimary),
+                    32.kh,
+                    BarWidget()
+                  ],
+                ),
+              ),
             ),
-          ),
+            Positioned(
+                right: 15,
+                bottom: 15,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    context.push(ChatPage());
+                  },
+                  child: Icon(
+                    Icons.chat,
+                    color: context.colors.onPrimary,
+                  ),
+                ))
+          ],
         ),
       ),
     );
